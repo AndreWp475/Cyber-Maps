af73cbdcd77551bcf1275f52518948b317982c57
AndreWp475 <55647411+AndreWp475@users.noreply.github.com>
2019-09-30 11:24:59 +0700

    Add files via upload

---
+++ CyberTeamMaps.sh
@@ -0,0 +1,183 @@
+#!/system/xbin/bash
+
+#LuGakBisaCoding
+#LaluEluRecoding?
+#MaluAnjing!
+
+clear
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet Welcome | lolcat
+echo "Peringatan...Gunakan Tools Saya Dengan Bijak :) " |lolcat
+sleep 1
+echo ""
+echo "=======================================" |lolcat
+echo          "Welcome Author : Mr.Andre.Wp              " |lolcat
+echo          "My Youtube     : AndreGamingWp            " |lolcat
+echo          "My Website     : fightercxshacking.com " |lolcat
+echo          "My Instagram   : @andre_wp475            " |lolcat
+echo          "My Warning       : No Recode My Tools  " |lolcat
+echo "=======================================" |lolcat
+echo ""
+echo "#<<<~DAFTAR ISI~>>>#" |lolcat
+echo "--------------------------------------" |lolcat
+echo "#<<Link Perang Cyber-Maps Dunia>>#" |lolcat
+echo ""
+echo "============================" | lolcat
+echo "1.  Link-Kasper-Sky"
+echo "============================" | lolcat
+echo "2.  Link-Fire-Eye"
+echo "============================" | lolcat
+echo "3.  Link-World-Map"
+echo "============================" | lolcat
+echo "4.  Link-Threat-Map"
+echo "============================" | lolcat
+echo "0.  Exit"
+echo "============================" | lolcat
+echo ""
+echo "╭─Pilih Bos" |lolcat
+read -p "╰─#" pil;
+
+if [ $pil = 1 ]
+then
+clear
+figlet Welcome.. |lolcat
+echo "Link Nya Di Bawah Ini Guys" |lolcat
+sleep 3
+echo ""
+echo ">>> https://cybermap.kaspersky.com/ <<<" |lolcat
+echo ""
+echo "Di Copy Dulu Link Di Atas Guys" |lolcat
+echo ""
+echo "Paste Kan Ke Chrome Atau Google Guys" |lolcat
+echo ""
+echo "Dan Terlihat Lah Perang Sengit" |lolcat
+echo ""
+echo "CYBER-TEAM Antar Negara(^_^)" |lolcat
+sleep 3
+clear
+figlet WARNING |lolcat
+echo ""
+echo "!!! Lagi Butuh Kasih Sayang Dan Pelukan !!! (^_^)" |lolcat
+echo ""
+echo "Jangan Lupa Support Channel YouTube Saya Ya Guys(•_•)" |lolcat
+echo ""
+echo "Created Tools By : Andre_Wp_Pku" |lolcat
+echo ""
+echo "Klik Tombol LIKE And SUBSCRIBE Ya Guys" |lolcat
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet Welcome.. |lolcat
+echo "Link Nya Di Bawah Ini Guys" |lolcat
+sleep 3
+echo ">>> https://www.fireeye.com/cyber-map/threat-map.html <<<" |lolcat
+echo ""
+echo "Di Copy Dulu Link Di Atas Guys" |lolcat
+echo ""
+echo "Paste Kan Ke Chrome Atau Google Guys" |lolcat
+echo ""
+echo "Dan Terlihat Lah Perang Sengit" |lolcat
+echo ""
+echo "CYBER-TEAM Antar Negara(^_^)" |lolcat
+sleep 3
+clear
+figlet WARNING |lolcat
+echo ""
+echo "!!! Lagi Butuh Kasih Sayang Dan Pelukan !!! (^_^)" |lolcat
+echo ""
+echo "Jangan Lupa Support Channel YouTube Saya Ya Guys(•_•)" |lolcat
+echo ""
+echo "Created Tools By : Andre_Wp_Pku" |lolcat
+echo ""
+echo "Klik Tombol LIKE And SUBSCRIBE Ya Guys" |lolcat
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet Welcome.. |lolcat
+echo "Link Nya Di Bawah Ini Guys" |lolcat
+sleep 3
+echo ">>> https://worldmap3.f-secure.com/ <<<" |lolcat
+echo ""
+echo "Di Copy Dulu Link Di Atas Guys" |lolcat
+echo ""
+echo "Terus Paste Kan Ke Chrome Atau Google Guys" |lolcat
+echo ""
+echo "Dan Terlihat Lah Perang Sengit" |lolcat
+echo ""
+echo "CYBER-TEAM Antar Negara(^_^)" |lolcat
+sleep 3
+clear
+figlet WARNING |lolcat
+echo ""
+echo "!!! Lagi Butuh Kasih Sayang Dan Pelukan !!! (^_^)" |lolcat
+echo ""
+echo "Jangan Lupa Support Channel YouTube Saya Ya Guys(•_•)" |lolcat
+echo ""
+echo "Created Tools By : Andre_Wp_Pku" |lolcat
+echo ""
+echo "Klik Tombol LIKE And SUBSCRIBE Ya Guys" |lolcat
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet Welcome.. |lolcat
+echo "Link Nya Di Bawah Ini Ya Guys" |lolcat
+sleep 3
+echo ">>> https://threatmap.checkpoint.com/ <<<" |lolcat
+echo ""
+echo "Di Copy Dulu Link Di Atas Guys" |lolcat
+echo ""
+echo "Terus Paste Kan Ke Chrome Atau Google Guys" |lolcat
+echo ""
+echo "Dan Terlihat Lah Perang Sengit" |lolcat
+echo ""
+echo "CYBER-TEAM Antar Negara(^_^)" |lolcat
+sleep 3
+clear
+figlet WARNING |lolcat
+echo ""
+echo "!!! Lagi Butuh Kasih Sayang Dan Pelukan !!! (^_^)" |lolcat
+echo ""
+echo "Jangan Lupa Support Channel YouTube Saya Ya Guys(•_•)" |lolcat
+echo ""
+echo "Created Tools By : Andre_Wp_Pku" |lolcat
+echo ""
+echo "Klik Tombol LIKE And SUBSCRIBE Ya Guys" |lolcat
+fi
+
+if [ $pil = 0 ]
+then
+clear
+figlet Thanks You |lolcat
+sleep 2
+echo "Makasih Guyss Udah Makai Tools Saya(^_^)" |lolcat
+echo ""
+sleep 1
+echo "Semoga Berfaedah Ya Guyss:)" |lolcat
+echo ""
+sleep 1
+echo "Jangan Lupa Support Channel Saya Guys(•_•)" |lolcat
+echo ""
+sleep 1
+echo "Klik Tombol LIKE Komen,SHARE And SUBSCRIBE Ya Guys" |lolcat
+exit
+fi
